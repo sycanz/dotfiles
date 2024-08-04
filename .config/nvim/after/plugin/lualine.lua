11c18bdd3dd9e7f613c2232e5903fdc83d169bc1
sycanz04 <aidenchan0397@gmail.com>
2024-08-04 21:32:11 +0800

    style: edit some components

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,40 +1,62 @@
 require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'horizon',
-    component_separators = {right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
+    options = {
+        icons_enabled = true,
+        -- theme = 'horizon',
+        -- component_separators = { left = '', right = '' }, -- 
+        component_separators = {},
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        }
     },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'filename'},
+        lualine_c = {
+            'branch',
+            'diff',
+            {
+                'diagnostics',
+                sources = { 'nvim_diagnostic', 'coc' },
+
+                -- Displays diagnostics for the defined severity types
+                sections = { 'error', 'warn', 'info'},
+
+                diagnostics_color = {
+                    -- Same values as the general color option can be used here.
+                    error = 'DiagnosticError', -- Changes diagnostics' error color.
+                    warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+                    info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+                },
+                symbols = { error = ' ', warn = ' ', info = ' ' },
+                colored = true,           -- Displays diagnostics status in color if set to true.
+                update_in_insert = false, -- Update diagnostics in insert mode.
+                always_visible = false,   -- Show diagnostics even if there are none.
+            }
+        },
+        lualine_x = {'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
 }
