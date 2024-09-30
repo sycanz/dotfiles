d081d7dab9bcf420f99094f730de43de095a06d3
sycanz04 <aidenchan0397@gmail.com>
2024-09-30 17:38:50 +0800

    style: edit file preview layout

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,15 +1,21 @@
 require('telescope').setup{
-  pickers = {
-    live_grep = {
-      theme = "dropdown",
+  defaults = {
+    layout_strategy = 'horizontal',
+    layout_config = {
+      horizontal = {
+        height = 0.7,
+        width = 0.9,
+        prompt_position = 'top',
+        preview_width = 0.60,
+
+      },
     },
-    find_files = {
-      theme = "dropdown",
-      previewer = false
-    },
+    -- sorting_strategy = "ascending"
   },
 }
 
 local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>gt', builtin.git_status, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
